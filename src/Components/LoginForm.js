4b4b6d07e8e699ff959736034037112fb1f46072
Alphashow <lastblast@outlook.fr>
2022-04-23 08:47:13 +0100

    Created and adjusted the form fields.

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,7 +1,19 @@
 const LoginForm = () => {
     return ( 
         <div className="login-form">
-            <p>login form</p>
+            <h1>Welcome back</h1>
+            <p>Welcome back ! please enter your details.</p>
+            <form>
+                <label>Email</label>
+                <input type="text" required/>
+                <label>Password</label>
+                <input type="password" required/>
+                <div className="password-options">
+                    <p>Remmember for 30 days</p>
+                    <a href="/">Forgot password</a>
+                </div>
+                <button>Login</button>
+            </form>
         </div>
      );
 }
