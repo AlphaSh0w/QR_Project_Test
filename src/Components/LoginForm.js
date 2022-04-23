907b67ec4ff72ca3ffd1aa22c74fbe791fdbab2f
Alphashow <lastblast@outlook.fr>
2022-04-23 09:17:52 +0100

    Added checkbox to remmember password.

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+    const [remmemberPass, setRemmemberPass] = useState(false);
+
     return ( 
         <div className="login-form">
             <h1>Welcome back</h1>
@@ -9,11 +13,20 @@ const LoginForm = () => {
                 <label>Password</label>
                 <input type="password" required/>
                 <div className="password-options">
-                    <p>Remmember for 30 days</p>
+                    <label>
+                        <input
+                              type="checkbox"
+                              name="remmemberPass"
+                              checked={remmemberPass}
+                              onChange={(e) => setRemmemberPass(e.target.checked)}
+                        />
+                        Remmember password
+                    </label>
                     <a href="/">Forgot password</a>
                 </div>
                 <button>Login</button>
             </form>
+            <p>Don't have an account ? <a href="/">Sign up</a></p>
         </div>
      );
 }
