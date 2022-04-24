dcee30f30a4696f840ecb0c3ddd0338d4b232bc3
Alphashow <lastblast@outlook.fr>
2022-04-24 23:48:33 +0100

    Fixed some text.

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -5,9 +5,9 @@ const Register = () => {
     return ( 
         <div className="register-page">
             <div className="left">
-                <h1>Title</h1>
+                <h1>Awesome website</h1>
                 <RegisterForm/>
-                <h3>Watermark</h3>
+                <h3>BOUCHELAGHEM Abdelghani Yanis 2022</h3>
             </div>
             <div className="right">
                 <div className="image"></div>
