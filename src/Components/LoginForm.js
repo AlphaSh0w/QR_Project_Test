735499acce07c3635ce08a1f291c478fb8477a05
Alphashow <lastblast@outlook.fr>
2022-04-24 18:06:35 +0100

    Added controlled inputs to all inputs.

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -4,15 +4,23 @@ import { Link } from "react-router-dom";
 
 const LoginForm = () => {
     const [remmemberPass, setRemmemberPass] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return ( 
         <div className="login-form">
             <h1>Welcome back</h1>
             <p>Welcome back ! please enter your details.</p>
             <form>
                 <label>Email</label>
-                <input type="text" required/>
+                <input type="text"
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+                required/>
                 <label>Password</label>
-                <input type="password" required/>
+                <input type="password"
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+                required/>
                 <div className="password-options">
                     <label>
                         <input
--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
     return ( 
         <div className="register-form">
             <h1>Register</h1>
             <p>Please enter your details to register.</p>
             <form>
                 <label>Email</label>
-                <input type="text" required/>
+                <input type="text"
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+                required/>
                 <label>Password</label>
-                <input type="password" required/>
+                <input type="password"
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+                required/>
                 <label>Password confirmation</label>
-                <input type="password" required/>
+                <input type="password"
+                value={passwordConfirm}
+                onChange={(e) => {setPasswordConfirm(e.target.value)}}
+                required/>
                 <button>Register</button>
             </form>
             <p>Already have an account ? <Link to="/login">Login</Link></p>
